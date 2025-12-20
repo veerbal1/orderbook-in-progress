1ea69331e9bdc85ce0595f23cd7e916fc2a6ba9c
veerbal1 <veerbalsingh1@gmail.com>
2025-12-20 13:06:01 +0530

    Add basic_u64 macro implementation in quantities module
    - Introduced a macro `basic_u64` to implement the `WrapperU64` trait for user-defined types.
    - Added methods for creating new instances, retrieving inner values, and implementing arithmetic operations (addition and subtraction).
    - Implemented the `Display` trait for formatted output of the inner value.

--- src/quantities.rs
+++ src/quantities.rs
@@ -1,4 +1,46 @@
+use std::fmt::Display;
+use std::ops::{Add, Sub};
+
 pub trait WrapperU64 {
     fn new(value: u64) -> Self;
     fn as_u64(&self) -> u64;
+}
+
+macro_rules! basic_u64 {
+    ($type_name:ident) => {
+        impl WrapperU64 for $type_name {
+            fn new(value: u64) -> Self {
+                Self { inner: value }
+            }
+
+            fn as_u64(&self) -> u64 {
+                self.inner
+            }
+        }
+
+        impl $type_name {
+            pub const ZERO: Self = $type_name { inner: 0 };
+            pub const ONE: Self = $type_name { inner: 1 };
+        }
+
+        impl Add for $type_name {
+            type Output = Self;
+            fn add(self, other: Self) -> Self {
+                $type_name::new(self.inner + other.inner)
+            }
+        }
+
+        impl Sub for $type_name {
+            type Output = Self;
+            fn sub(self, other: Self) -> Self {
+                $type_name::new(self.inner - other.inner)
+            }
+        }
+
+        impl Display for $type_name {
+            fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+                self.inner.fmt(f)
+            }
+        }
+    };
 }
